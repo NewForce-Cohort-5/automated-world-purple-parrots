2f391b115b6ed587591492e7876c621d3d16befd
heavenburdette1988 <heavenburdette1988@gmail.com>
2021-10-20 15:09:48 -0400

    finishing homepage

--- HomePage/homePage.js
+++ HomePage/homePage.js
@@ -11,7 +11,7 @@ const tuvalu = () => { document.querySelector(".tuvalu").innerHTML = `
 const madagascar = () => { document.querySelector(".madagascar").innerHTML = `
 <div class="">
                     <h2>Madagaskar</h2>
-                <a href="mad.html"><img src="https://www.telegraph.co.uk/content/dam/Travel/2017/june/cycling%20131314241_Cultura%20Exclusive_Madagascar-xlarge.jpg?imwidth=640"  class="img"></a>  
+                <a href="mad.html"><img src="https://www.telegraph.co.uk/content/dam/Travel/2017/june/cycling%20131314241_Cultura%20Exclusive_Madagascar-xlarge.jpg?imwidth=640" alt="trees in Madagaskar"  class="img"></a>  
                 <p>Madagascar is big. It’s 226,917 square miles to be precise, making it the fourth largest island on the planet (and bigger than Spain, Thailand, Sweden and Germany). The UK is a rather puny 93,410 square miles in comparison. Which is the world’s largest island? Take the quiz below to find out.</p>            
                 </div>
 `
@@ -19,9 +19,10 @@ const madagascar = () => { document.querySelector(".madagascar").innerHTML = `
  
 const argentina = () => { document.querySelector(".argentina").innerHTML = `
 <div class="">
-                    <h2>This is a country</h2>
-                <a href="Argentina.html"><img src="images/image3.jpg" width="100" height="100" class="img"></a>  
-                <p>Some text about the country.</p>            
+                    <h2>Argentina</h2>
+                <a href="Argentina.html"><img src="https://www.planetware.com/wpimages/2020/02/argentina-in-pictures-beautiful-places-to-photograph-perito-moreno-glacier.jpg" alt="Glaicer in Argentina" class="img"></a>  
+                <p>"That's what they do in Argentina. Have a little wine and talk. Then have some coffee and talk. Then, go back to the wine."
+                -Grace Jones</p>            
                 </div>
 `
 }
